b4d02ff626ef74490800bd1b36361e65db8aa761
runmeng <mengrun.bfu@gmail.com>
2017-06-03 10:32:35 -0400

    Version of June 3rd

---
+++ src/main/java/spring/proj1/controller/GifController.java
@@ -0,0 +1,14 @@
+package spring.proj1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifts(){
+        return  "home";
+    }
+
+}
